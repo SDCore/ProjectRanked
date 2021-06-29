9bd5bb27c94a17f913edb23336d359e72d5455a3
SDCore <sdcored@gmail.com>
2021-06-29 04:30:06 -0500

    Soon.

--- index.php
+++ index.php
@@ -1,1 +1,28 @@
 <DOCTYPE html>
+
+<html>
+
+<head>
+    <style>
+        body {
+            margin: 0;
+            padding: 0;
+            background: #000;
+        }
+
+        soon {
+            color: #FFF;
+            margin-top: 15%;
+            font-size: 50pt;
+            text-align: center;
+        }
+    </style>
+</head>
+
+<body>
+
+    <p class="soon">Soon.</p>
+
+</body>
+
+</html>
