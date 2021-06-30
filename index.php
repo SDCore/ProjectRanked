3bcf712829002859fbd556d93cf65418df424b93
SDCore <sdcored@gmail.com>
2021-06-29 19:24:04 -0500

    Navbar

--- index.php
+++ index.php
@@ -9,7 +9,27 @@
 </head>
 
 <body>
-apex ranked leadeboard
+    <nav class="nav">
+        <div class="inner">
+            <a class="brand" href="/">Apex Ranked</a>
+            <a class="link active" href="/">Home</a>
+            <a class="link" href="#">Player Search</a>
+            <a class="link" href="#">About</a>
+            <a class="link" href="#">FAQ</a>
+        </div>
+    </nav>
+
+    <!-- content -->
+    top 750 on pc - top 750 on ps4 - top 750 on xbox
+
+    <!-- news -->
+    apex news
+
+    <!-- featured stream(?) -->
+    featured twitch stream
+
+    <!-- footer -->
+    footer - copyright
 </body>
 
 </html>
