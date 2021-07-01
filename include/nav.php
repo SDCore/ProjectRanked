e40f4a41776a3368c9283d9959819f23b387846e
SDCore <sdcored@gmail.com>
2021-07-01 16:23:10 -0500

    Navbar Styling + Includes

---
+++ include/nav.php
@@ -0,0 +1,20 @@
+<DOCTYPE html>
+
+<html>
+
+<head>
+    <title>Apex Legends Ranked Leaderboard</title>
+
+    <link type="text/css" rel="stylesheet" href="./css/main.css" />
+</head>
+
+<body>
+    <nav class="nav">
+        <div class="inner">
+            <a class="brand" href="/"><span class="text">Apex Ranked</span></a>
+            <a class="link <?= ($_SERVER['SCRIPT_NAME']=="/index.php") ? 'active':''; ?>" href="/"><span class="text">Home</span></a>
+            <a class="link <?= ($_SERVER['SCRIPT_NAME']=="/search.php") ? 'active':''; ?>" href="#"><span class="text">Player Search</span></a>
+            <a class="link <?= ($_SERVER['SCRIPT_NAME']=="/about.php") ? 'active':''; ?>" href="#"><span class="text">About</span></a>
+            <a class="link <?= ($_SERVER['SCRIPT_NAME']=="/faq.php") ? 'active':''; ?>" href="#"><span class="text">FAQ</span></a>
+        </div>
+    </nav>
--- index.php
+++ index.php
@@ -1,23 +1,4 @@
-<DOCTYPE html>
-
-<html>
-
-<head>
-    <title>Apex Legends Ranked Leaderboard</title>
-
-    <link type="text/css" rel="stylesheet" href="./css/main.css" />
-</head>
-
-<body>
-    <nav class="nav">
-        <div class="inner">
-            <a class="brand" href="/">Apex Ranked</a>
-            <a class="link active" href="/">Home</a>
-            <a class="link" href="#">Player Search</a>
-            <a class="link" href="#">About</a>
-            <a class="link" href="#">FAQ</a>
-        </div>
-    </nav>
+<?php require_once("./include/nav.php"); ?>
 
     <!-- content -->
     top 750 on pc - top 750 on ps4 - top 750 on xbox
@@ -28,8 +9,4 @@
     <!-- featured stream(?) -->
     featured twitch stream
 
-    <!-- footer -->
-    footer - copyright
-</body>
-
-</html>
+<?php require_once("./include/footer.php"); ?>
