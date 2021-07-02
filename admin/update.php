84d5b5a231d42739467a94fd9cc341a21da44f82
SDCore <sdcored@gmail.com>
2021-07-02 17:45:49 -0500

    Database Stuff

---
+++ admin/update.php
@@ -0,0 +1,43 @@
+<?php
+
+// Hi :) If you're viewing this, please don't super-reverse-engineer it
+// and ruin it. I haven't set up a way to authenticate/rate limit, and I
+// don't want to if I don't have to. This is the simplist way for me to
+// update the rank info, and while I don't mind having to complicate it,
+// I really shouldn't have to. Please keep that in mind.
+//
+// - Love, SDCore <3
+
+include "../connect.php";
+set_time_limit(72000);
+
+// Create and check connection
+$DBConn = mysqli_connect($host, $user, $pass, $db);
+
+if(!$DBConn) {
+  die("Error: Connection failed. " . mysqli_connect_error());
+}
+
+$playerCount = mysqli_fetch_array($DBConn->query("SELECT COUNT(*) FROM projectRanked"));
+
+for($i = 1; $i < $playerCount[0] + 1; $i++) {
+  $getPlayer = "SELECT * FROM projectRanked WHERE id = $i";
+  $queryPlayer = mysqli_query($DBConn, $getPlayer);
+
+  while($row = mysqli_fetch_array($queryPlayer)) {
+    $url = "https://api.apexstats.dev/stats.php?platform=".$row['Platform']."&player=".$row['PlayerName'];
+
+    $getJson = file_get_contents($url);
+
+    $json = json_decode($getJson, true);
+
+    mysqli_query($DBConn, "UPDATE projectRanked SET PlayerName = '".$json['userData']['username']."', lastUpdated = '".time()."' WHERE PlayerID = '1008500432960'");
+
+    // mysqli_query($DBConn, "UPDATE projectRanked SET `PlayerName` = '".$json['userData']['username']."', `lastUpdated` = '".$row['id']." + 1' WHERE `PlayerID` = '".$json['userData']['userID']."'");
+
+    sleep(2);
+  }
+
+  //if($i == $playerCount[0] + 1)
+  //  break;
+}
--- index.php
+++ index.php
@@ -13,34 +13,35 @@
         <div class="title">Socials</div>
     </div>
 
-    <div class="item">
-        <div class="list level">Level 1,000</div>
-        <div class="list name">[PC] Zuni_Tv</div>
-        <div class="list rank">Master</div>
-        <div class="list rp">10,000</div>
-        <div class="list socials">(Twitch) (Twitter)</div>
-    </div>
-    <div class="item">
-        <div class="list level">Level 1,000</div>
-        <div class="list name">[PC] Zuni_Tv</div>
-        <div class="list rank">Master</div>
-        <div class="list rp">10,000</div>
-        <div class="list socials">N/A</div>
-    </div>
-    <div class="item">
-        <div class="list level">Level 1,000</div>
-        <div class="list name">[PC] Zuni_Tv</div>
-        <div class="list rank">Master</div>
-        <div class="list rp">10,000</div>
-        <div class="list socials">N/A</div>
-    </div>
-    <div class="item">
-        <div class="list level">Level 1,000</div>
-        <div class="list name">[PC] Zuni_Tv</div>
-        <div class="list rank">Apex Predator</div>
-        <div class="list rp">10,000</div>
-        <div class="list socials">N/A</div>
-    </div>
+    <?php
+        $DBConn = mysqli_connect($host, $user, $pass, $db);
+        $rankedQuery = mysqli_query($DBConn, "SELECT * FROM projectRanked ORDER BY `BR_RankScore` DESC");
+
+        function isPred($pred) {
+            if($pred == 0) return "Master";
+
+            return "Apex Predator";
+        }
+
+        function formatSocial($name, $type) {
+            if($name == "N/A") return;
+
+            if($type == "Twitter") return "<a href='https://twitter.com/".$name."'>Twitter</a>";
+            if($type == "Twitch") return "<a href='https://twitch.tv/".$name."'>Twitch</a>";
+
+            return;
+        }
+
+        while($row = mysqli_fetch_assoc($rankedQuery)) {
+            echo '<div class="item">';
+                echo '<div class="list"><img src="https://i.imgur.com/vp64kDF.png" class="icon" /> '.$row['PlayerLevel'].'</div>';
+                echo '<div class="list"><a href="#">'.$row['PlayerName'].'</a></div>';
+                echo '<div class="list">'.isPred($row['BR_isPred']).'</div>';
+                echo '<div class="list">'.$row['BR_RankScore'].' RP</div>';
+                echo '<div class="list">'.formatSocial($row['Twitter'], "Twitter").' '.formatSocial($row['Twitch'], "Twitch").'</div>';
+            echo '</div>';
+        }
+    ?>
 </div>
 
     <!-- content -->
