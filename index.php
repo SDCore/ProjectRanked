deb388f10f139fce6acb9bb3d0e6bcec5b12e511
SDCore <sdcored@gmail.com>
2021-07-01 19:31:55 -0500

    List Stuff

--- index.php
+++ index.php
@@ -6,16 +6,40 @@
 
 <div class="container">
     <div class="item">
-        <div class="title">Rank</div>
         <div class="title">Account Level</div>
-        <div class="title">Name/Platform</div>
+        <div class="title">Name</div>
+        <div class="title">Rank</div>
         <div class="title">RP</div>
+        <div class="title">Socials</div>
     </div>
 
     <div class="item">
-        <div class="list">1 2 3 4 5</div>
-        <div class="list">1 2 3 4 5</div>
-        <div class="list">1 2 3 4 5</div>
+        <div class="list level">Level 1,000</div>
+        <div class="list name">[PC] Zuni_Tv</div>
+        <div class="list rank">Master</div>
+        <div class="list rp">10,000</div>
+        <div class="list socials">(Twitch) (Twitter)</div>
+    </div>
+    <div class="item">
+        <div class="list level">Level 1,000</div>
+        <div class="list name">[PC] Zuni_Tv</div>
+        <div class="list rank">Master</div>
+        <div class="list rp">10,000</div>
+        <div class="list socials">N/A</div>
+    </div>
+    <div class="item">
+        <div class="list level">Level 1,000</div>
+        <div class="list name">[PC] Zuni_Tv</div>
+        <div class="list rank">Master</div>
+        <div class="list rp">10,000</div>
+        <div class="list socials">N/A</div>
+    </div>
+    <div class="item">
+        <div class="list level">Level 1,000</div>
+        <div class="list name">[PC] Zuni_Tv</div>
+        <div class="list rank">Apex Predator</div>
+        <div class="list rp">10,000</div>
+        <div class="list socials">N/A</div>
     </div>
 </div>
 
