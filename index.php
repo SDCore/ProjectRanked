76423f70302b76c7fc36b97e76bc0965c2e83224
SDCore <sdcored@gmail.com>
2021-07-02 19:46:41 -0500

    Some Style Changes

--- index.php
+++ index.php
@@ -49,12 +49,12 @@
 </div>
 
     <!-- content -->
-    top 750 on pc - top 750 on ps4 - top 750 on xbox
+    <!-- top 750 on pc - top 750 on ps4 - top 750 on xbox -->
 
     <!-- news -->
-    apex news
+    <!-- apex news -->
 
     <!-- featured stream(?) -->
-    featured twitch stream
+    <!-- featured twitch stream -->
 
 <?php require_once("./include/footer.php"); ?>
