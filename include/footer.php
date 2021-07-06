94cc455a7f10bcbd56c57a82ff383316d9edd7c8
SDCore <sdcored@gmail.com>
2021-07-06 00:27:58 -0500

    Add User in Admin

--- include/footer.php
+++ include/footer.php
@@ -1,7 +1,7 @@
     <!-- Footer -->
     <footer class="footer">
         <div class="inner">
-            <b>&copy; SDCore <?php echo date("Y"); ?>. All rights reserved. &middot; Data updated every 6 hours.</b> <?php if(isset($_SESSION['user'])) { echo "<span style='float: right;'>".$_SESSION['username']." &middot; <a href='../admin/' style='color: rgba(255, 255, 255, 0.5); text-decoration: none;'>Admin</a> &middot; <a href='../logout' style='color: rgba(255, 255, 255, 0.5); text-decoration: none;'>Sign Out</a></span>"; } ?>
+            <b><a href="/admin/login" style="text-decoration: none; color: rgba(255, 255, 255, 0.25);">&copy;</a> SDCore <?php echo date("Y"); ?>. All rights reserved. &middot; Data updated every 6 hours.</b> <?php if(isset($_SESSION['user'])) { echo "<span style='float: right;'>".$_SESSION['username']." &middot; <a href='../admin/' style='color: rgba(255, 255, 255, 0.5); text-decoration: none;'>Admin</a> &middot; <a href='../logout' style='color: rgba(255, 255, 255, 0.5); text-decoration: none;'>Sign Out</a></span>"; } ?>
         </div>
     </footer>
 
