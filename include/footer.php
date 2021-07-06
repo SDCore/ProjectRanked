e57742d164bf9225e778db6fe193ba9cdb5b20bd
SDCore <sdcored@gmail.com>
2021-07-06 03:26:38 -0500

    Footer Stuff

--- include/footer.php
+++ include/footer.php
@@ -1,7 +1,18 @@
+<?php 
+    include_once("../connect.php");
+    $DBConn = mysqli_connect($host, $user, $pass, $db);
+
+    $recentQuery = mysqli_query($DBConn, "SELECT * FROM projectRanked ORDER BY `BR_RankScore` ASC");
+
+    while ($row = mysqli_fetch_assoc($recentQuery)) {
+        $time = $row['lastUpdated'];
+    }
+?>
+    
     <!-- Footer -->
     <footer class="footer">
         <div class="inner">
-            <b><a href="/admin/login" style="text-decoration: none; color: rgba(255, 255, 255, 0.25);">&copy;</a> SDCore <?php echo date("Y"); ?>. All rights reserved. &middot; Data updated every 6 hours.</b> <?php if(isset($_SESSION['user'])) { echo "<span style='float: right;'>".$_SESSION['username']." &middot; <a href='../admin/' style='color: rgba(255, 255, 255, 0.5); text-decoration: none;'>Admin</a> &middot; <a href='../logout' style='color: rgba(255, 255, 255, 0.5); text-decoration: none;'>Sign Out</a></span>"; } ?>
+            <b><a href="/admin/login" style="text-decoration: none; color: rgba(255, 255, 255, 0.25);">&copy;</a> SDCore <?php echo date("Y"); ?>. &middot; Data updated every 6 hours. &middot; Last updated <?php echo strftime("%m-%d-%Y, %H:%M:%S", $time); ?> UTC.</b> <?php if(isset($_SESSION['user'])) { echo "<span style='float: right;'>".$_SESSION['username']." &middot; <a href='../admin/' style='color: rgba(255, 255, 255, 0.5); text-decoration: none;'>Admin</a> &middot; <a href='../logout' style='color: rgba(255, 255, 255, 0.5); text-decoration: none;'>Sign Out</a></span>"; } ?>
         </div>
     </footer>
 
