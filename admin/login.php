4526324e828f55ae13aef56907201bd16bb03b9b
SDCore <sdcored@gmail.com>
2021-07-06 15:06:11 -0500

    Remove Error Logging

--- admin/login.php
+++ admin/login.php
@@ -1,8 +1,5 @@
 <?php
     require_once("../include/nav.php");
-    ini_set('display_errors', '1');
-    ini_set('display_startup_errors', '1');
-    error_reporting(E_ALL);
 
     $username_err = $password_err = $username = "";
 
