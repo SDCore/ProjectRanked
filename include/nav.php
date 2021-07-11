c62002b7d37c2e569a0367c0778bad76cb5003eb
SDCore <sdcored@gmail.com>
2021-07-11 04:12:49 -0500

    Some Style Updates

--- include/nav.php
+++ include/nav.php
@@ -19,8 +19,8 @@
         <div class="inner">
             <a class="brand" href="/"><span class="text">Apex Ranked</span></a>
             <a class="link <?= ($_SERVER['SCRIPT_NAME']=="/index.php") ? 'active':''; ?>" href="/"><span class="text">Home</span></a>
-            <a class="link <?= ($_SERVER['SCRIPT_NAME']=="/search.php") ? 'active':''; ?>" href="/search"><span class="text">Player Search</span></a>
+            <!-- <a class="link <?= ($_SERVER['SCRIPT_NAME']=="/search.php") ? 'active':''; ?>" href="/search"><span class="text">Player Search</span></a>
             <a class="link <?= ($_SERVER['SCRIPT_NAME']=="/about.php") ? 'active':''; ?>" href="/about"><span class="text">About</span></a>
-            <a class="link <?= ($_SERVER['SCRIPT_NAME']=="/faq.php") ? 'active':''; ?>" href="/faq"><span class="text">FAQ</span></a>
+            <a class="link <?= ($_SERVER['SCRIPT_NAME']=="/faq.php") ? 'active':''; ?>" href="/faq"><span class="text">FAQ</span></a> -->
         </div>
     </nav>
