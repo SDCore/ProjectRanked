0b416cf798180a01ee7aabc5ca0237c7372a95b0
SDCore <sdcored@gmail.com>
2021-07-16 17:25:47 -0500

    Added Shadow to Error Message

--- user.php
+++ user.php
@@ -6,7 +6,7 @@
     $getPlayer = mysqli_query($DBConn, "SELECT * FROM `projectRanked` WHERE `PlayerID` = '$PID'");
     $player = mysqli_fetch_assoc($getPlayer);
 
-    if(mysqli_num_rows($getPlayer) < 1) { echo '<div style="font-size: 25pt; width: 100%; text-align: center; color: #FFF; margin-top: 25px; font-weight: bold;">User does not exist.</div>'; }
+    if(mysqli_num_rows($getPlayer) < 1) { echo '<div style="font-size: 25pt; width: 100%; text-align: center; color: #FFF; margin-top: 25px; font-weight: bold; text-shadow: 0 4px 0 rgba(0, 0, 0, 0.9);">User does not exist.</div>'; }
 ?>
 
 <?php require_once("./include/footer.php"); ?>
