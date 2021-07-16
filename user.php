7ef234d89e1b083177f9f35b8fd0638549f8b564
SDCore <sdcored@gmail.com>
2021-07-16 17:22:13 -0500

    Small Fix

--- user.php
+++ user.php
@@ -6,9 +6,7 @@
     $getPlayer = mysqli_query($DBConn, "SELECT * FROM `projectRanked` WHERE `PlayerID` = '$PID'");
     $player = mysqli_fetch_assoc($getPlayer);
 
-    if(mysqli_num_rows($getPlayer) < 1) { echo '<div style="font-size: 25pt; width: 100%; text-align: center; color: #FFF; margin-top: 25px; font-weight: bold;">User does not exist.</div>'; return; }
-
-    echo "test";
+    if(mysqli_num_rows($getPlayer) < 1) { echo '<div style="font-size: 25pt; width: 100%; text-align: center; color: #FFF; margin-top: 25px; font-weight: bold;">User does not exist.</div>'; }
 ?>
 
 <?php require_once("./include/footer.php"); ?>
