f828b18dbfd274bb4ae178a79e06a811af5ac09d
SDCore <sdcored@gmail.com>
2021-07-18 16:27:29 -0500

    Begin Refactor

--- about.php
+++
@@ -1,5 +0,0 @@
-<?php $pageTitle = "About"; require_once("./include/nav.php"); ?>
-
-    <center style="color: #FFF; font-size: 25pt; margin-top: 15px;">Soon.</center>
-
-<?php require_once("./include/footer.php"); ?>
