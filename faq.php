35bc92102aa60098474f96f7633c335e8a50065a
SDCore <sdcored@gmail.com>
2021-11-20 02:31:02 -0600

    Nav Styling

---
+++ faq.php
@@ -0,0 +1,6 @@
+<?php
+    $title = "Home";
+    require_once("./include/nav.php");
+?>
+
+<center><h1>Soon</h1></center>
--- include/nav.php
+++ include/nav.php
@@ -1,4 +1,21 @@
-<?php require_once(__DIR__."/../connect.php"); ?>
+<?php 
+    require_once(__DIR__."/../connect.php");
+
+    $DBConn = mysqli_connect($host, $user, $pass, $db);
+
+    $NavInfo = mysqli_fetch_array(mysqli_query($DBConn, "SELECT * FROM seasonInfo")) or die(mysqli_error($DBConn));
+
+    function active($page) {
+        if ($_SERVER['SCRIPT_NAME'] == $page.".php") return "active";
+
+        return "link";
+    }
+
+    function rankType($type) {
+        if($type == "BR") return "Battle Royale";
+        if($type == "Arenas") return "Arenas";
+    }
+?>
 
 <!DOCTYPE html>
 
@@ -15,8 +32,8 @@
 
     <?php
         if($debug == false) {
-            if($GoogleAnalytics == "BR") require_once(__DIR__."/../analytics/BR.html");
-            if($GoogleAnalytics == "Arenas") require_once(__DIR__."/../analytics/Arenas.html");
+            if($RankType == "BR") require_once(__DIR__."/../analytics/BR.html");
+            if($RankType == "Arenas") require_once(__DIR__."/../analytics/Arenas.html");
         }
     ?>
 </head>
@@ -24,16 +41,15 @@
 <body>
     <nav class="nav">
         <a href="https://ranked.apexstats.dev/" class="brand">
-            <span class="text">
-                <span class="top">Season Name</span>
-                <span class="bottom">Ranked Type</span>
+            <span class="inner">
+                <span class="top"><?php echo $NavInfo['name']; ?></span>
+                <span class="bottom"><?php echo rankType($RankType); ?></span>
             </span>
         </a>
-        <a href="#">Home</a>
-        <!-- <a href="#">Home</a> -->
-        <!-- <a href="#">History</a> -->
-        <!-- <a href="#">Search</a> -->
-        <!-- <a href="#">F.A.Q.</a> -->
-        <!-- <a href="#">Submit</a> -->
-        <!-- <a href="#">Discord</a> -->
+        <a href="/" class="<?php echo active('/index'); ?>"><span class="inner">Home</span></a>
+        <a href="#" class="link disabled"><span class="inner">History</span></a>
+        <a href="#" class="link disabled"><span class="inner">Search</span></a>
+        <a href="/faq" class="<?php echo active('/faq'); ?>"><span class="inner">F.A.Q.</span></a>
+        <a href="#" class="link disabled"><span class="inner">Submit</span></a>
+        <a href="#" class="link disabled"><span class="inner">Discord</span></a>
     </nav>
