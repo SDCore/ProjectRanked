b8a26e375791ae526968b1e953df6eed6cc727c8
SDCore <sdcored@gmail.com>
2021-11-26 04:29:20 -0600

    FAQ Info/Styling

--- faq.php
+++ faq.php
@@ -3,4 +3,25 @@
     require_once("./include/nav.php");
 ?>
 
-<center><h1>Soon</h1></center>
+<div class="header">
+    <div class="top">Frequently Asked Questions</div>
+</div>
+
+<div class="container">
+    <div class="faq">
+        <div class="question">
+            I don't see myself on the leaderboard. Is there some way I could get myself added?
+        </div>
+        <div class="answer">
+            Currently, there is no way on the website to add yourself to the leaderboard, but that is being worked on. If you would like to be added manually, hit me up on Discord (SDCore#0001) or Twitter (@SDCore_) with your Apex username and the platform that you play on, and I'll add you.
+        </div>
+    </div>
+    <div class="faq">
+        <div class="question">
+            Why does the leaderboard not show any rank under Gold?
+        </div>
+        <div class="answer">
+            Most of the playerbase is going to be Plat+, so I don't see too much value in showing anything below that rank. There are people added to the database that are Gold or below, they just don't show up. This might be changed in the future, but for now, it's mainly a leaderboard for people looking to grind ranked and see where they place against other similar players.
+        </div>
+    </div>
+</div>
