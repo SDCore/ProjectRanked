9342a19c20878b2d51f396e10df7fa6485b2e814
SDCore <sdcored@gmail.com>
2021-11-29 16:48:08 -0600

    Updated FAQ

--- faq.php
+++ faq.php
@@ -24,4 +24,12 @@
             Most of the playerbase is going to be Plat+, so I don't see too much value in showing anything below that rank. There are people added to the database that are Gold or below, they just don't show up. This might be changed in the future, but for now, it's mainly a leaderboard for people looking to grind ranked and see where they place against other similar players.
         </div>
     </div>
+    <div class="faq">
+        <div class="question">
+            Why do certain people show as unranked in earlier rank periods, while others have their rank on their profile?
+        </div>
+        <div class="answer">
+            I (currently) manually add each person to the database, so in earlier ranked seasons, there were less people than there are in later/current rank seasons. So the fact that they show as "Unranked" could be because they weren't in the database at that time, or they simply didn't play ranked for that season.
+        </div>
+    </div>
 </div>
