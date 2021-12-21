b1b01b6aa0f8674a77a1fccff28195c9d3a6424f
SDCore <sdcored@gmail.com>
2021-12-21 11:36:46 -0600

    Update Current Split Info

--- include/header.php
+++ include/header.php
@@ -16,7 +16,7 @@
 
 <div class="header">
     <div class="top">
-        <?php echo platformText(); ?> Ranked Stats for <?php echo $SeasonInfo['name']; ?>: Split <?php echo $SeasonInfo['split']; ?>
+        <?php echo platformText(); ?> Ranked Stats for <?php echo $SeasonInfo['name']; ?>: Split <?php echo $SeasonInfo['currentSplit']; ?>
     </div>
     <div class="bottom">
     <?php echo scoreType($RankType); ?> Threshold for Apex Predator Based on <?php echo $totalRows; ?> Players: <b><?php echo number_format(minPred($minPred[$DBRankScore], $RankType, $DBRankScore, $RankFile['Master']))." ".scoreType($RankType); ?></b>
