52e5dfff1bda86b183d9cac3e56907ef5b88a49a
SDCore <sdcored@gmail.com>
2022-04-23 13:27:28 -0500

    Added Split End Time to Header

--- include/header.php
+++ include/header.php
@@ -12,6 +12,18 @@
             return $min;
         }
     }
+
+    function timeTextHeader($time, $text) {
+        if($time > 1 || $time == 0) return $time." ".$text."\s";
+
+        return $time." ".$text;
+    }
+
+    function splitTimestamp($time) {
+        $timestamp = $time - time();
+
+        return gmdate(timeTextHeader("d", "\d\a\y").", ".timeTextHeader("h", "\h\o\u\\r").", ".timeTextHeader("i", "\m\i\\n\u\\t\\e"), $timestamp);
+    }
 ?>
 
 <div class="header">
@@ -19,7 +31,7 @@
     <div class="middle"><b><?= $SeasonInfo['name']; ?></b> &#8212; Split <?= $SeasonInfo['currentSplit']; ?></div>
     <div class="stats">
         <span class="threshold">Predator Threshold: <?= number_format(minPred($minPred[$DBRankScore], $RankType, $DBRankScore, $RankFile['Master'])); ?> <?= scoreType($RankType); ?></span>
-        <!-- <span class="predCount">Apex Predator Count: <?= $predCount; ?></span> -->
+        <span class="splitTime"><?= splitTimestamp($SeasonInfo['end']); ?></span>
         <span class="playerCount">Based on <?= $totalRows; ?> <?= platformText() ?> Players</span>
     </div>
 </div>
