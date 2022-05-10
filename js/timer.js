d3819d290e24bc758a456cd0fa52c52dc6912cfe
SDCored <sdcored@gmail.com>
2022-05-09 23:55:09 -0500

    Logic for Split/End Times on Countdown

--- js/timer.js
+++ js/timer.js
@@ -11,12 +11,19 @@ function timer() {
 		.then((response) => response.json())
 		.then((data) => {
 			var time = new Date().getTime() / 1000;
-			var end = data.dates.end - time;
+			var dates = data.dates;
+			var split = data.season.currentSplit;
 
-			var days = Math.floor(end / (60 * 60 * 24));
-			var hours = Math.floor(end / (60 * 60));
-			var minutes = Math.floor((end / 60) % 60);
-			var seconds = Math.floor(end % 60);
+			if (split == 1) {
+				var current = dates.split - time;
+			} else {
+				var current = dates.end - time;
+			}
+
+			var days = Math.floor(current / (60 * 60 * 24));
+			var hours = Math.floor(current / (60 * 60));
+			var minutes = Math.floor((current / 60) % 60);
+			var seconds = Math.floor(current % 60);
 
 			var countdown = `${plural(days, "day")}, ${plural(
 				hours,
